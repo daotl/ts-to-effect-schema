e8934d4fec5be9d60491397671139a5392bd4880
zhaoweijie <zwjCheer@163.com>
2024-01-30 11:46:09 +0800

    chore: upgrade effect @effect/schema

--- src/core/transformRecursiveSchema.test.ts
+++ src/core/transformRecursiveSchema.test.ts
@@ -1,52 +1,51 @@
-import ts from 'typescript'
-import { findNode } from '../utils/findNode'
-import { transformRecursiveSchema } from './transformRecursiveSchema'
+import ts from "typescript";
+import { findNode } from "../utils/findNode";
+import { transformRecursiveSchema } from "./transformRecursiveSchema";
 
-describe('transformRecursiveSchema', () => {
-  it('should wrap the variable declaration with the appropriate syntax', () => {
+describe("transformRecursiveSchema", () => {
+  it("should wrap the variable declaration with the appropriate syntax", () => {
     const sourceFile = ts.createSourceFile(
-      'index.ts',
+      "index.ts",
       `export const categorySchema = S.struct({
       name: S.string,
       subcategories: S.array(categorySchema),
     })`,
-      ts.ScriptTarget.Latest,
-    )
+      ts.ScriptTarget.Latest
+    );
 
-    const declaration = findNode(sourceFile, ts.isVariableStatement)
+    const declaration = findNode(sourceFile, ts.isVariableStatement);
     if (!declaration) {
-      fail('should have a variable declaration')
+      fail("should have a variable declaration");
     }
 
-    const output = transformRecursiveSchema('S', declaration, 'Category')
+    const output = transformRecursiveSchema("S", declaration, "Category");
 
-    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
 
-    expect(
-      printer.printNode(ts.EmitHint.Unspecified, output, sourceFile),
-    ).toMatchInlineSnapshot(`
-      "export const categorySchema: ObjectSchema<Category> = S.lazy(() => S.struct({
+    expect(printer.printNode(ts.EmitHint.Unspecified, output, sourceFile))
+      .toMatchInlineSnapshot(`
+      "export const categorySchema: ObjectSchema<Category> = S.suspend(() => S.struct({
           name: S.string,
           subcategories: S.array(categorySchema),
       }));"
-    `)
-  })
+    `);
+  });
 
-  it('should throw if the statement is not valid', () => {
+  it("should throw if the statement is not valid", () => {
     const sourceFile = ts.createSourceFile(
-      'index.ts',
+      "index.ts",
       `export const categorySchema;
     })`,
-      ts.ScriptTarget.Latest,
-    )
+      ts.ScriptTarget.Latest
+    );
 
-    const declaration = findNode(sourceFile, ts.isVariableStatement)
+    const declaration = findNode(sourceFile, ts.isVariableStatement);
     if (!declaration) {
-      fail('should have a variable declaration')
+      fail("should have a variable declaration");
     }
 
     expect(() =>
-      transformRecursiveSchema('S', declaration, 'Category'),
-    ).toThrowErrorMatchingInlineSnapshot(`"Unvalid effect statement"`)
-  })
-})
+      transformRecursiveSchema("S", declaration, "Category")
+    ).toThrowErrorMatchingInlineSnapshot(`"Unvalid effect statement"`);
+  });
+});
